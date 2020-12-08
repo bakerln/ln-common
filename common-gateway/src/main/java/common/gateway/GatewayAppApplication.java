a9fec6db4846811757816d3ba9738b3825f3cca4
李楠 <linan.groot@outlook.com>
2020-12-08 11:01:22 +0800

    gateway

--- common-gateway/src/main/java/common/gateway/GatewayAppApplication.java
+++ common-gateway/src/main/java/common/gateway/GatewayAppApplication.java
@@ -1,10 +1,8 @@
 package common.gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>Description:  网关启动</p>
